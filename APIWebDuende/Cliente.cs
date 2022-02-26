86a0d78125a95fd0d6714341a8ca3700f4ec5a45
Gerardo <Villalobos>
2022-02-26 09:17:26 -0600

    .

---
+++ APIWebDuende/Cliente.cs
@@ -0,0 +1,23 @@
+﻿namespace APIWebDuende
+{
+    using Duende.IdentityServer;
+    using Duende.IdentityServer.Models;
+
+    public class Cliente
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+            {
+                new Client()
+                {
+                    ClientId = "mtwdm2022",
+                    ClientName = "Ejemplo de implementación de autenticación con Duende Identity Server",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = new List<Secret> { new Secret("superpassword".Sha256()) },
+                    AllowedScopes = new List<string> { "ApiWebDuende" }
+                },
+            };
+        }
+    }
+}
---
+++ APIWebDuende/Resources.cs
@@ -0,0 +1,47 @@
+﻿namespace APIWebDuende
+{
+    using Duende.IdentityServer.Models;
+    public class Resources
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource
+                {
+                    Name = "role",
+                    UserClaims = new List<string> {"role"}
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[]
+            {
+                new ApiResource
+                {
+                    Name = "api1",
+                    DisplayName = "Api1",
+                    Description = "Allow the application to access Api1",
+                    Scopes = new List<string> {"ApiWebDuende"},
+                    ApiSecrets = new List<Secret> {new Secret("ScopeSecret".Sha256())},
+                    UserClaims = new List<string> {"role"}
+                }
+            };
+        }
+
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new[]
+            {
+                new ApiScope("ApiWebDuende", "Access to Api1")
+
+            };
+        }
+    }
+
+}
